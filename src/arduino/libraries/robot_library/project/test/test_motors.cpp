540d4fe117d596c7324c09dfbac4b536dd3aa06c
Raul Perula-Martinez <raul@raul-laptop.(none)>
2014-12-10 17:37:02 +0100

    added tests

---
+++ src/arduino/libraries/robot_library/project/test/test_motors.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file        test_motors.cpp
+ * @brief       Main.
+ *
+ * @author      Raul Perula-Martinez <raul.perula@uc3m.es>
+ * @date        2014-12
+ *
+ * @version     1.0.0
+ * @license     GPLv3
+ * @copyright   Asociacion de Robotica, University Carlos III of Madrid
+ */
+
+#include "../servomotor_driver.h"
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
